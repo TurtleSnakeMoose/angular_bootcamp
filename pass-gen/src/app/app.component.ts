c620952c53cb6154337fc77c516f290e9ac8017a
Michael Roskin <michael.roskin@nucleix.com>
2022-08-07 14:37:32 +0300

    done with password generator

--- pass-gen/src/app/app.component.ts
+++ pass-gen/src/app/app.component.ts
@@ -5,6 +5,48 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pass-gen';
+
+  title = 'Password Generator';
+  length = 5
+  result = ''
+  useLetters = false
+  useNumbers = false
+  useSymbols = false
+
+  onPasswordLengthChange (length: number) {
+    this.length = length
+  }
+
+  onChangeUseLetters () {
+    this.useLetters = !this.useLetters
+  }
+  
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers
+  }
+  
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols
+  }
+
+  onButtonClick () {
+
+    this.result = ''
+    const letters = this.useLetters ? 'abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ' : ''
+    const numbers = this.useNumbers ? '1234567890' : ''
+    const symbols = this.useSymbols ? '~!@#$%^&*()_+|{}<>?/:;' : ''
+
+    const validChars = `${letters}${numbers}${symbols}`
+    
+    for (let index = 0; index < this.length; index++) {
+      this.result += validChars[Math.floor(Math.random() * validChars.length)]
+    }
+  } 
+
+  isMissingInfo() {
+    return (!this.useLetters && !this.useNumbers && !this.useSymbols) || this.length <= 0
+  }
 }
+
